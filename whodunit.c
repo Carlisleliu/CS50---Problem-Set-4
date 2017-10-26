8bfa02cc200cf9721f22d77eeacf7ee17b35bab0
Liu Jiawei <carlisle_liu@hotmail.com>
2017-10-26 14:39:33 +0800

    Create whodunit.c

---
+++ whodunit.c
@@ -0,0 +1,48 @@
+***
+#include <cs50.h>
+#include <stdio.h>
+
+#include "bmp.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: ./whodunit infile outfile\n");
+        return 1;
+    }
+    
+    char *infile = argv[1];
+    char *outfile = argv[2];
+    
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s\n", infile);
+        return 2;
+    }
+    
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not open %s\n", outfile);
+        return 3;
+    }
+    
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+    
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+    
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+        bi.biBitCount != 24 || bi.biCompression != 0)
+        {
+            fclose(outptr);
+            fclose(inptr);
+            fprintf(stderr, "Unsupported file format.\n");
+            return 4;
+        }
+}
+***
